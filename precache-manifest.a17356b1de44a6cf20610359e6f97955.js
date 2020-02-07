a0ff53f094bd065a7b89b3e9d254173ace54931d
Andrew <an.smodlev@gmail.com>
2020-02-07 19:17:14 +0300

    Updates

---
+++ precache-manifest.a17356b1de44a6cf20610359e6f97955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430990b68a2985661fe21c039e5a5104",
+    "url": "/it-logger/index.html"
+  },
+  {
+    "revision": "b70ebe04776461d04afc",
+    "url": "/it-logger/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "8cf886d8e087f83f70ae",
+    "url": "/it-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b70ebe04776461d04afc",
+    "url": "/it-logger/static/js/2.6e21e369.chunk.js"
+  },
+  {
+    "revision": "f805a2ee8624fa0613eb6008f42d0b97",
+    "url": "/it-logger/static/js/2.6e21e369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf886d8e087f83f70ae",
+    "url": "/it-logger/static/js/main.1b3d5610.chunk.js"
+  },
+  {
+    "revision": "7b327e69ba733697541f",
+    "url": "/it-logger/static/js/runtime-main.508321c4.js"
+  }
+]);
